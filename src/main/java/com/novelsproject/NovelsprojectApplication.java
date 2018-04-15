0897c94e34617f7f642f59af33b9bc411d1f9f0c
Samir Santos <samirsilva16@gmail.com>
2018-04-15 16:45:53 -0300

    Projeto Criado

---
+++ src/main/java/com/novelsproject/NovelsprojectApplication.java
@@ -0,0 +1,12 @@
+package com.novelsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelsprojectApplication.class, args);
+	}
+}
