9fdf4045cc98b00abada50ebf1a1fd924232cd33
Samir Santos <samirsilva16@gmail.com>
2018-04-15 17:06:40 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/novelsproject/resources/CategoriasResources.java
+++ src/main/java/com/novelsproject/resources/CategoriasResources.java
@@ -1,15 +1,28 @@
 package com.novelsproject.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.novelsproject.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriasResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Funcional";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Horror");
+		Categoria cat2 = new Categoria(2, "Action");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
